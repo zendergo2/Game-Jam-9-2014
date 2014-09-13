6128354aa40fb4bc3e159a326a0ae3d2d665b3da
zendergo2 <zendergo2@gmail.com>
2014-09-13 12:02:24 -0500

    Mechanics v2
    Jumping is not buggy anymore.

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -4,93 +4,132 @@ using System.Collections;
 public class CharacterMove : MonoBehaviour {
 
 	public float moveSpeed;
+	public float jumpSpeed;
 	public float jumpHeight;
 	public float gravity;
-	private Vector2 currSpeed;
-	private Vector2 currGravity;
-	private Vector2 currJump;
+	public bool jumping;
+
 	private bool canJump;
+	private Vector2 xInput;
+	private Vector2 yInput;
+	private Vector2 yGravity;
+	private bool canMoveLeft;
+	private bool canMoveRight;
+	private bool canMoveUp;
+	private bool canMoveDown;
 
-	// FixedUpdate is called once per physics frame
-	void Update ()
+	private int layerMask = 1 << 8;
+
+	void Start()
 	{
-		currSpeed = Input.GetAxis ("Horizontal") * Vector2.right * moveSpeed * Time.deltaTime;
-		currGravity = -Vector2.up * gravity * Time.deltaTime;
-		currJump = Input.GetAxis ("Vertical") * Vector2.up * jumpHeight;
+		canMoveLeft = false;
+		canMoveRight = false;
+		canMoveUp = false;
+		canMoveDown = false;
+		jumping = false;
+		canJump = false;
 
-		if (noCollideXRight() && currSpeed.x >= 0)
-			transform.Translate (currSpeed);
-		else if (noCollideXLeft() && currSpeed.x <= 0)
-			transform.Translate (currSpeed);
-
-		if (noCollideYUp() && currJump.y > 0 && canJump)
-			jump(currJump);
-		else if (noCollideYDown())
-			transform.Translate (currGravity);
+		layerMask = ~layerMask;
 	}
 
-	bool noCollideXRight()
+	void FixedUpdate()
 	{
-		Vector2 originTop = new Vector2(transform.position.x + 0.51f, transform.position.y + 0.51f);
-		Vector2 originBottom = new Vector2(transform.position.x + 0.51f, transform.position.y - 0.51f);
-		RaycastHit2D hitTop = Physics2D.Raycast(originTop, Vector2.right, 0.01f);
-		RaycastHit2D hitBottom = Physics2D.Raycast(originBottom, Vector2.right, 0.01f);
+		xInput = Input.GetAxis ("Horizontal") * Vector2.right * moveSpeed * Time.deltaTime;
+		yInput = Input.GetAxisRaw ("Vertical") * Vector2.up * jumpSpeed * Time.deltaTime;
+		yGravity = -Vector2.up * gravity * Time.deltaTime;
 
-		if (hitBottom.collider == null && hitTop.collider == null)
-			return true;
-		else if (hitBottom.collider.tag == "Moveable" || hitTop.collider.tag == "Moveable")
-			return true;
-		else
-			return false;
+		if (canMoveLeft && xInput.x < 0)
+			moveLeft(xInput);
+		else if (canMoveRight && xInput.x > 0)
+			moveRight(xInput);
+
+		if (canMoveUp && yInput.y > 0 & canJump)
+		{
+			canJump = false;
+			float startPos = transform.position.y;
+			StartCoroutine(jump(yInput, startPos));
+		}
+		else if (canMoveDown && !jumping)
+			enableGravity();
+
+		checkPosition();
 	}
 
-	bool noCollideXLeft()
+	/**
+	 * Raycatsing to check position
+	 */
+	void checkPosition()
 	{
-		Vector2 originTop = new Vector2(transform.position.x - 0.51f, transform.position.y + 0.51f);
-		Vector2 originBottom = new Vector2(transform.position.x - 0.51f, transform.position.y - 0.51f);
-		RaycastHit2D hitTop = Physics2D.Raycast(originTop, -Vector2.right, 0.01f);
-		RaycastHit2D hitBottom = Physics2D.Raycast(originBottom, -Vector2.right, 0.01f);
-		
-		if (hitBottom.collider == null && hitTop.collider == null)
-			return true;
-		else if (hitBottom.collider.tag == "Moveable" || hitTop.collider.tag == "Moveable")
-			return true;
+		Vector2 castTRU = new Vector2(transform.position.x + 0.47f, transform.position.y + 0.47f);
+		Vector2 castTLU = new Vector2(transform.position.x - 0.47f, transform.position.y + 0.47f);
+		Vector2 castTLL = new Vector2(transform.position.x - 0.47f, transform.position.y + 0.47f);
+		Vector2 castBLL = new Vector2(transform.position.x - 0.47f, transform.position.y - 0.47f);
+		Vector2 castBLD = new Vector2(transform.position.x - 0.47f, transform.position.y - 0.47f);
+		Vector2 castBRD = new Vector2(transform.position.x + 0.47f, transform.position.y - 0.47f);
+		Vector2 castBRR = new Vector2(transform.position.x + 0.47f, transform.position.y - 0.47f);
+		Vector2 castTRR = new Vector2(transform.position.x + 0.47f, transform.position.y + 0.47f);
+
+		RaycastHit2D hitTRU = Physics2D.Raycast(castTRU, Vector2.up, 0.1f, layerMask);
+		RaycastHit2D hitTLU = Physics2D.Raycast(castTLU, Vector2.up, 0.1f, layerMask);
+		RaycastHit2D hitTLL = Physics2D.Raycast(castTLL, -Vector2.right, 0.1f, layerMask);
+		RaycastHit2D hitBLL = Physics2D.Raycast(castBLL, -Vector2.right, 0.1f, layerMask);
+		RaycastHit2D hitBLD = Physics2D.Raycast(castBLD, -Vector2.up, 0.1f, layerMask);
+		RaycastHit2D hitBRD = Physics2D.Raycast(castBRD, -Vector2.up, 0.1f, layerMask);
+		RaycastHit2D hitBRR = Physics2D.Raycast(castBRR, Vector2.right, 0.1f, layerMask);
+		RaycastHit2D hitTRR = Physics2D.Raycast(castTRR, Vector2.right, 0.1f, layerMask);
+
+		if (hitTRU.collider == null && hitTLU.collider == null)
+			canMoveUp = true;
 		else
-      		return false;
-  	}
-
-	bool noCollideYDown()
-	{
-		Vector2 originRight = new Vector2(transform.position.x + 0.51f, transform.position.y - .501f);
-		Vector2 originLeft = new Vector2(transform.position.x - 0.51f, transform.position.y - .501f);
-		RaycastHit2D hitRight = Physics2D.Raycast(originRight, -Vector2.up, 0.1f);
-		RaycastHit2D hitLeft = Physics2D.Raycast(originLeft, -Vector2.up, 0.1f);
-
-		if (hitRight.collider == null && hitLeft.collider == null)
-			return true;
+			canMoveUp = false;
+		if (hitTLL.collider == null && hitBLL.collider == null)
+			canMoveLeft = true;
+		else
+			canMoveLeft = false;
+		if (hitBLD.collider == null && hitBRD.collider == null)
+			canMoveDown = true;
 		else
 		{
+			canMoveDown = false;
 			canJump = true;
-			return false;
+		}
+		if (hitBRR.collider == null && hitTRR.collider == null)
+			canMoveRight = true;
+		else
+			canMoveRight = false;
+	}
+
+	void moveLeft(Vector2 xInput)
+	{
+		transform.Translate (xInput);
+	}
+
+	void moveRight(Vector2 xInput)
+	{
+		transform.Translate (xInput);
+	}
+
+	IEnumerator jump(Vector2 yInput, float startPos)
+	{
+		while (true)
+		{
+			float height = transform.position.y - startPos;
+			if (height >= jumpHeight)
+			{
+				jumping = false;
+				StopAllCoroutines();
+			}
+			else if (height < jumpHeight)
+			{
+				jumping = true;
+				transform.Translate(yInput);
+			}
+			yield return new WaitForFixedUpdate();
 		}
 	}
 
-	bool noCollideYUp()
+	void enableGravity()
 	{
-		Vector2 originRight = new Vector2(transform.position.x + 0.51f, transform.position.y + .51f);
-		Vector2 originLeft = new Vector2(transform.position.x - 0.51f, transform.position.y + .51f);
-		RaycastHit2D hitRight = Physics2D.Raycast(originRight, Vector2.up, 0.1f);
-		RaycastHit2D hitLeft = Physics2D.Raycast(originLeft, Vector2.up, 0.1f);
-		
-		if (hitRight.collider == null && hitLeft.collider == null)
-			return true;
-		else
-			return false;
-	}
-
-	void jump(Vector2 currJump)
-	{
-		transform.Translate (currJump);
-		canJump = false;
+		transform.Translate(yGravity);
 	}
 }
